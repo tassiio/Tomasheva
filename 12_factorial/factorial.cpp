e6ae15297c08cabc445defc6cf3af36e95a243cf
Nastya <nastik124@gmail.com>
2021-09-17 08:56:27 +0300

    factorial is added

---
+++ 12_factorial/factorial.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+
+int input()
+{
+    while(true)
+    {
+        int int_num;
+
+        cout << "Enter an integer positive number less than 100000:\n";
+        cin >> int_num;
+
+        if(cin.fail() || int_num < 0 || int_num > 100000)
+        {
+            cin.clear();
+            cin.ignore(32767, '\n');
+
+            cout << "Invalid input. Please, enter a number.\n";
+        }
+        
+        else
+            {
+                int_num = static_cast<int>(int_num); // type conversion into int if the input is not integer
+                cout << "Your number isn't integer. So I have converted it into " << int_num << ".\n";
+                return int_num;
+            }
+    }
+}
+
+long long int factorial(int num)
+{
+    long long int Factorial_n = 1;
+    if (num != 0)
+    {
+        for (int i = 1; i < num + 1; i++)
+        {
+        Factorial_n = Factorial_n * i;
+        }
+        return Factorial_n;
+    }
+
+    else
+        return Factorial_n;
+}
+
+int main()
+{
+    int number = input();
+    // cout << "Number is " << number << endl;
+    long long int factorial_of_a_number = factorial(number);
+    
+    cout << "Factorial of " << number << " is " << factorial_of_a_number << endl;
+    return 0;
+}
---
+++ 13_Fibonacci_numbers/Fibonacci.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+int input()
+{
+    while(true)
+    {
+        int int_num;
+
+        cout << "Enter an integer positive number less than 100:\n";
+        cin >> int_num;
+
+        if(cin.fail() || int_num < 0 || int_num > 100)
+        {
+            cin.clear();
+            cin.ignore(32767, '\n');
+
+            cout << "Invalid input. Please, enter a number.\n";
+        }
+        
+        else
+            {
+                int_num = static_cast<int>(int_num); // type conversion into int if the input is not integer
+                cout << "Your number isn't integer. So I have converted it into " << int_num << ".\n";
+                return int_num;
+            }
+    }
+}
+
+int Fibonacci_numbers(int num)
+{
+    if (num == 0)
+        return 0;
+    
+    else if (num == 1)
+        return 1;
+
+    else 
+        return Fibonacci_numbers(num-2) + Fibonacci_numbers(num-1);
+}
+
+int main()
+{
+    int number = input();
+    // cout << "Number is " << number << endl;
+    
+    cout << "There are Fibonacci numbers: ";
+
+    for(int i = 0; i < number; i++)
+        {
+            cout << Fibonacci_numbers(i) << " ";
+        }
+
+    cout << endl;
+    return 0;
+}
